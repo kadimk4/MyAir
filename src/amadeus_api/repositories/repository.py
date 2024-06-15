428b6dff2b7ff60763488bc01793dca298618e4d
Yuriy Ilchuk <yuryilchuk@yandex.ru>
2024-06-15 23:45:54 +0700

    amadeus_repo

--- src/amadeus_api/repositories/repository.py
+++ src/amadeus_api/repositories/repository.py
@@ -1,3 +1,4 @@
+from amadeus import Hotel
 from api.main import amadeus
 
 
@@ -32,11 +33,58 @@ class TravelRepository:
     def __init__(self) -> None:
         self.request = amadeus.travel
 
-    def predict_travel_pupose(self, city_code_from: str, city_code_to: str, departure_date: str, return_date: str, search_date: str):
+    def predict_travel_pupose(self, city_code_from: str, city_code_to: str, departure_date: str, arrival_date: str, search_date: str):
         return self.request.predictions.trip_purpose.get(
             originLocationCode=city_code_from,
             destinationLocationCode=city_code_to,
             departureDate=departure_date,
-            returnDate=return_date,
+            returnDate=arrival_date,
             searchDate=search_date
         )
+
+    def predinct_flight_delay(self, city_code_from: str, city_code_to: str, departure_date: str, departure_time: str, arrival_date: str, arrival_time: str, aircraft_code: str, carrier_code: str, flight_number: str, duration: str):
+        return self.request.predictions.flight_delay.get(
+            originLocationCode=city_code_from,
+            destinationLocationCode=city_code_to,
+            departureDate=departure_date,
+            departureTime=departure_time,
+            arrivalDate=arrival_date,
+            arrivalTime=arrival_time,
+            aircraftCode=aircraft_code,
+            carrierCode=carrier_code,
+            flightNumber=flight_number,
+            duration=duration
+        )
+
+
+class ReferenceDataRepository:
+
+    def __init__(self) -> None:
+        self.request = amadeus.reference_data
+
+    def details_airport(self, airport_code: str):
+        return self.request.location(airport_code).get()
+
+    def list_near_airports(self, longitude: float, latitude: float):
+        return self.request.locations.airports.get(longitude=longitude, latitude=latitude)
+
+    def checkin_links(self, airline_code: str, language: str = 'en-GB'):
+        return self.request.urls.checkin_links.get(airlineCode=airline_code, language=language)
+
+    def recommended_locations(self, city_code: str, country_code: str):
+        return self.request.recommended_locations.get(cityCodes=city_code, travelerCountryCode=country_code)
+
+    def list_city_hotels(self, city_code: str):
+        return self.request.locations.hotel.get(keyword=city_code, subType=[Hotel.HOTEL_LEISURE, Hotel.HOTEL_GDS])
+
+
+class AirportRepository:
+
+    def __init__(self) -> None:
+        self.request = amadeus.airport
+
+    def percentage_ofontime_departures(self, airport_code: str, date: str):
+        return self.request.predictions.on_time.get(airportCode=airport_code, date=date)
+
+    def airport_direct(self, airport_code: str):
+        return self.request.direct_destinations.get(airport_code)
