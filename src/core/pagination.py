7ff465a69270fd8745de1fc8c4b166e2c78956c5
Yuriy Ilchuk <yuryilchuk@yandex.ru>
2024-06-18 04:10:40 +0700

    pagination expansion

--- src/core/pagination.py
+++ src/core/pagination.py
@@ -2,6 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class BasePagination(PageNumberPagination):
-    page_size = 2
+    page_size = 20
     page_query_param = 'page_size'
-    max_page_size = 2
+    max_page_size = 100
